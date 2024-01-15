e9df261c4bb39bfde6ed986b076ba1f4ebe1697a
Deiiivy <deivypr28@gmail.com>
2024-01-14 21:16:10 -0500

    add About component

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/about.css'
+
+function About() {
+  return (
+    <div className='container-about'>
+      <h2>About</h2>
+      <p>I am a passionate lover of software development with a deep knowledge and a solid background in programming logic.
+         and a solid background in programming logic. With my experience and skills, 
+         I aspire to face technical challenges with creativity and solve problems with precision. </p>
+    </div>
+  )
+}
+
+export default About
