74796002af9390687aa55efefd03623b1ccc4d36
Deiiivy <deivypr28@gmail.com>
2024-01-14 21:20:56 -0500

    add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+function Footer() {
+  return (
+    <div className='footer'>
+  <a href="https://twitter.com/Deivy66592439">
+    <box-icon name='twitter' type='logo' color='#ffffff'></box-icon>
+  </a>
+  <a href="https://www.facebook.com/profile.php?id=100073572857475&locale=es_LA">
+    <box-icon type='logo' name='facebook-square' color='#ffffff'></box-icon>
+  </a>
+  <a href="https://www.instagram.com/deivy_pt/">
+    <box-icon name='instagram' type='logo' color='#ffffff'></box-icon>
+  </a>
+  <a href="https://github.com/Deiiivy">
+    <box-icon type='logo' name='github' color='#ffffff'></box-icon>
+  </a>
+</div>  
+  )
+}
+
+export default Footer
